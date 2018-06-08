69c225913941cc94b60bea680eb5b3bbade6c9bd
jgarcia-network <3106189+jgarcia-network@users.noreply.github.com>
2018-06-07 22:29:02 -0700

    Done

--- unit1/evennum.js
+++ unit1/evennum.js
@@ -1,3 +1,9 @@
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // Write a for loop that will print all the odd numbers in the numbers array.
+
+for (i=0; i <= numbers.length; i++){
+    if (numbers[i] % 2 === 0){
+        console.log(numbers[i]);
+    }
+}
