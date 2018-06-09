bdfa8cd999374a23d0c791557efba34fcb83fcaa
therogue <alex@roguemark.com>
2018-06-09 08:57:51 -0400

    Added FizzBuzz

--- unit1/evennum.js
+++ unit1/evennum.js
@@ -3,7 +3,7 @@ var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Write a for loop that will print all the odd numbers in the numbers array.
 
 for (i=0; i <= numbers.length; i++){
-    if (numbers[i] % 2 === 0){
+    if (numbers[i] % 2 === 0){  // Good solution! Could you print the odd numbers instead?
         console.log(numbers[i]);
     }
 }
---
+++ unit2/fizzbuzz.js
@@ -0,0 +1,14 @@
+// FizzBuzz is a popular programming problem. 
+
+// This program currently runs through the first 30 integers and prints them.
+
+// Change the program to print out the integers using the following rules instead:
+
+// 1. If the number is divisible by 3, print out 'Fizz' instead of the number.
+// 2. If the number is divisible by 5, print out 'Buzz' instead of the number.
+// 3. If the number is divisible by both 3 and 5, print out 'FizzBuzz' instead of the number.
+// 4. Otherwise, print the number.
+
+for (i=1; i <= 30; i++) {
+  console.log(i);
+}
