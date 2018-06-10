f0ad5dd05210d99298d5afad52e3d491a07d56ad
therogue <alex@roguemark.com>
2018-06-10 18:01:18 -0400

    Added fizzbuz fun

---
+++ unit3/fizzbuzz_fun.js
@@ -0,0 +1,5 @@
+// This is a version of the FizzBuzz program where the branching logic 
+// to determine what to print out for each number is placed inside a
+// function. The function should take one parameter (the number) and
+// use the if-else branching logic to return the appropriate string for that 
+// number.
