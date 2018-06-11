d63c31360b44a43a2288cfa5a2ea3b5f68fc6039
therogue <alex@roguemark.com>
2018-06-11 08:15:13 -0400

    Added exercises.

---
+++ unit3/sumfun.js
@@ -0,0 +1,3 @@
+// Write a program that sums up the first 10 numbers and prints out the result.
+// The program should contain a function with a for loop that adds up the numbers.
+// The function should take an int parameter n and return the sum of the first 10 numbers.
