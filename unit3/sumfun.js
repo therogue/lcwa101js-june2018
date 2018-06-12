39b16ae358f7aac18d2ba143b002eff9beb57354
therogue <alex@roguemark.com>
2018-06-12 07:34:15 -0400

    Corrected instructions.

--- unit3/sumfun.js
+++ unit3/sumfun.js
@@ -1,3 +1,3 @@
-// Write a program that sums up the first 10 numbers and prints out the result.
+// Write a program that sums up the first 10 natural numbers and prints out the result.
 // The program should contain a function with a for loop that adds up the numbers.
-// The function should take an int parameter n and return the sum of the first 10 numbers.
+// The function should take an int parameter n and return the sum of the first n numbers.
