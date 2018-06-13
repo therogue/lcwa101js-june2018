84756c60f8da1895f261c2a77fc04505b77d774d
therogue <alex@roguemark.com>
2018-06-13 06:36:44 -0400

    Added two assignments.

---
+++ unit4/numseries.js
@@ -0,0 +1,11 @@
+// Define a function numSeries that takes a number n and returns a string
+// that summarizes the first n positive integers as '1 2 3 ... n'.
+
+// See examples below.
+
+console.log(numSeries(12)); // 1 2 3 ... 12
+console.log(numSeries(7));  // 1 2 3 ... 7
+console.log(numSeries(6));  // 1 2 3 4 5 6
+console.log(numSeries(5));  // 1 2 3 4 5
+console.log(numSeries(2));  // 1 2
+
---
+++ unit4/vowels.js
@@ -0,0 +1,5 @@
+// Write a function numVowels that returns the number of vowels in a string.
+
+console.log(numVowels(''));
+console.log(numVowels('Phoenix, Arizona'));
+console.log(numVowels('Abracadabra'));
